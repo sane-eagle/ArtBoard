e3d0a2325030072f07992e6c0706ee61e9a18571
Yash Sawant <yashsawant868@gmail.com>
2026-01-12 09:54:01 +0530

    Update index.tsx

--- src/components/toolbox/index.tsx
+++ src/components/toolbox/index.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { useDispatch, useSelector } from "react-redux";
 import styles from "./index.module.css";
 import { COLORS, MENU_ITEMS } from "@/lib/constants";
@@ -9,19 +10,29 @@ import { getSocket } from "@/lib/socket";
 
 const Toolbox = () => {
   const dispatch = useDispatch();
-  const activeMenuItem = useSelector((state: ReduxState) => state.menu.activeMenuItem);
-  const { color, size } = useSelector((state: ReduxState) => state.toolbox[activeMenuItem]);
+  const socket = getSocket(); // ✅ SAFE (null in static deploy)
+
+  const activeMenuItem = useSelector(
+    (state: ReduxState) => state.menu.activeMenuItem
+  );
+  const { color, size } = useSelector(
+    (state: ReduxState) => state.toolbox[activeMenuItem]
+  );
+
   const showStrokeToolOption = activeMenuItem === MENU_ITEMS.PENCIL;
-  const showBrushToolOption = activeMenuItem === MENU_ITEMS.PENCIL || MENU_ITEMS.ERASER;
+  const showBrushToolOption =
+    activeMenuItem === MENU_ITEMS.PENCIL ||
+    activeMenuItem === MENU_ITEMS.ERASER;
 
   const updateBrushSize = (e: React.ChangeEvent<HTMLInputElement>) => {
-    dispatch(changeBrushSize({ item: activeMenuItem, size: parseInt(e.target.value) }));
-    socket.emit("changeConfig", { color, size: parseInt(e.target.value) });
+    const newSize = parseInt(e.target.value);
+    dispatch(changeBrushSize({ item: activeMenuItem, size: newSize }));
+    socket?.emit("changeConfig", { color, size: newSize });
   };
 
-  const updateColor = (color: string) => {
-    dispatch(changeColor({ item: activeMenuItem, color }));
-    socket.emit("changeConfig", { color, size });
+  const updateColor = (newColor: string) => {
+    dispatch(changeColor({ item: activeMenuItem, color: newColor }));
+    socket?.emit("changeConfig", { color: newColor, size });
   };
 
   return (
@@ -30,23 +41,37 @@ const Toolbox = () => {
         <div className={styles.toolboxItem}>
           <h4 className={styles.toolboxText}>Stroke Color</h4>
           <div className={styles.itemContainer}>
-            {/* {Object.keys(COLORS).map((color) => (
-              <div key={color} className={cx(styles.colorBox,{[]:styles.active})} style={{ backgroundColor: COLORS[color as keyof typeof COLORS] }} onClick={() => updateColor(COLORS[color as keyof typeof COLORS])} />
-            ))} */}
-            {Object.keys(COLORS).map((colorKey) => {
-              // Get the current color for this iteration
-              const currentColor = COLORS[colorKey as keyof typeof COLORS];
+            {Object.keys(COLORS).map((key) => {
+              const currentColor =
+                COLORS[key as keyof typeof COLORS];
 
-              return <div key={colorKey} className={cx(styles.colorBox, { [styles.active]: color === currentColor })} style={{ backgroundColor: currentColor }} onClick={() => updateColor(currentColor)} />;
+              return (
+                <div
+                  key={key}
+                  className={cx(styles.colorBox, {
+                    [styles.active]: color === currentColor,
+                  })}
+                  style={{ backgroundColor: currentColor }}
+                  onClick={() => updateColor(currentColor)}
+                />
+              );
             })}
           </div>
         </div>
       )}
+
       {showBrushToolOption && (
         <div className={styles.toolboxItem}>
           <h4 className={styles.toolboxText}>Brush Size</h4>
           <div className={styles.itemContainer}>
-            <input type="range" min={1} max={10} step={1} onChange={updateBrushSize} value={size} />
+            <input
+              type="range"
+              min={1}
+              max={10}
+              step={1}
+              onChange={updateBrushSize}
+              value={size}
+            />
           </div>
         </div>
       )}
